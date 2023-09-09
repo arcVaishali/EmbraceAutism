f212c2d4254752ce33e788290164032bae030657
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-09 20:54:22 +0530

    Update dependencies, add new features, remove unused files.

---
+++ src/components/MoveToTop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoveToTop = () => {
+  return (
+    <div>MoveToTop</div>
+  )
+}
+
+export default MoveToTop
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        righteous: ['Righteous', 'cursive'],
+        poppins: ['Poppins', 'sans'],
+      },
+      fontSize: {
+        '40': '40px',
+        '35': '35px',
+        '30': '30px',
+      },
+      colors: {
+        green: '#606C38',
+        'olive-green': '#283618',
+        soft: '#FEFAE0',
+        orange: '#DDA15E',
+        'dark-orange': '#BC6C25',
+        'light-blue': '#CAF3F0',
+        blue: '#1A82EE',
+      }
+    },
   },
   plugins: [],
 }
