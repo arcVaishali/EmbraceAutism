24f29cada0e3b3d5adb1c767c7a52638519e7697
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-10 15:06:18 +0530

    Update contact email and organization name in the footer

--- src/AutismAppComponents/Footer.js
+++ src/AutismAppComponents/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-[#12E0B7] to-[#07F76B] text-blue-500">
       <div className="py-8 px-6 sm:px-8 lg:px-12 mx-auto flex items-start justify-between min-w-7xl">
-        <div className='p-3'>
+        <div className='px-10'>
           <p className="font-bold text-lg mb-4">Embrace Autism</p>
           <p className="text-sm text-black mb-4">A vital resource for individuals on the autism spectrum </p>
           <p className="text-sm text-black mb-4">seeking empowerment, community engagement,  </p>
-          <p className="text-sm text-black mb-4"> and the opportunity to advocate for positive change. </p>
+          <p className="text-sm text-black mb-4">and the opportunity to advocate for positive change. </p>
           <div className="flex space-x-4">
             <Link to="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><SiFacebook className="text-2xl text-blue-500 transform hover:scale-110 transition-all duration-300" /></Link>
             <Link to="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><SiTwitter className="text-2xl text-blue-500 transform hover:scale-110 transition-all duration-300" /></Link>
@@ -29,13 +29,13 @@ const Footer = () => {
         <div className="flex-1">
           <p className="font-bold text-lg mb-4">Contact Us</p>
           <p className="text-sm text-black mb-4">123 Main Street, City</p>
-          <p className="text-sm text-black mb-4">Email: info@empowerasd.com</p>
+          <p className="text-sm text-black mb-4">Email: info@embraceautism.com</p>
           <p className="text-sm text-black">Phone: 123-456-7890</p>
         </div>
       </div>
 
       <div className="bg-[rgba(0,0,0,0.2)] py-4 px-6 sm:px-8 lg:px-12 flex items-center justify-between min-w-7xl">
-        <p className="text-sm text-black">© 2022 EmpowerASD. All rights reserved.</p>
+        <p className="text-sm text-black">© 2023 Embrace Autism. All rights reserved.</p>
         <p className="text-sm text-black">Privacy Policy | Terms of Service</p>
       </div>
     </footer>
