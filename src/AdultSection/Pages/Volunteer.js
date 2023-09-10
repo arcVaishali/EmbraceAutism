4eb82e52b389705c80f4b8e688a25cbf22ecc1a6
vaishali <vaishalirdj14@gmail.com>
2023-09-10 15:45:40 +0530

    minor changes made

--- src/AdultSection/Pages/Volunteer.js
+++ src/AdultSection/Pages/Volunteer.js
@@ -4,12 +4,12 @@ import VolunteeringEvent from '../components/VolunteeringEvent'
 
 const Volunteer = () => {
     return (
-        <div class="bg-gray-100" style={{backgroundColor:"#0d87f430"}}>
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                    <h2 class="text-4xl font-bold text-gray-900" style={{textAlign:"center"}}>Volunteer with us</h2>
+        <div className="bg-gray-100" style={{backgroundColor:"#0d87f430"}}>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+                    <h2 className="text-4xl font-bold text-gray-900" style={{textAlign:"center"}}>Volunteer with us</h2>
 
-                    <div class="mt-6 space-y-12 lg:grid lg:grid-cols-1 lg:gap-x-6 lg:space-y-0">
+                    <div className="mt-6 space-y-12 lg:grid lg:grid-cols-1 lg:gap-x-6 lg:space-y-0">
                         
                         <VolunteeringEvent/>
                         <VolunteeringEvent/>
--- src/AutismAppPages/AdultHome.js
+++ src/AutismAppPages/AdultHome.js
@@ -11,21 +11,21 @@ const AdultHome = () => {
       <Haze/>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-20">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             EmpowerASD: Unlocking Potential Together.
-            <Link to="/" class="font-semibold text-[#0D79F4]">
-              <span class="absolute inset-0" aria-hidden="true">
+            <Link to="/" className="font-semibold text-[#0D79F4]">
+              <span className="absolute inset-0" aria-hidden="true">
               </span>Read more <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
         </div>
 
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Advocacy Beyond Boundaries</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">A vital resource for adults on the autism spectrum who seek empowerment, community engagement, and the opportunity to advocate for positive change. </p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <Link to="/" class="rounded-md bg-[#0D79F4] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">See more</Link>
-            <Link to="/" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></Link>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Advocacy Beyond Boundaries</h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">A vital resource for adults on the autism spectrum who seek empowerment, community engagement, and the opportunity to advocate for positive change. </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link to="/" className="rounded-md bg-[#0D79F4] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">See more</Link>
+            <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></Link>
           </div>
         </div>
       </div>
