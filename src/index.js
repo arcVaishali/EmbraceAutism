42dd7aa86bd87fc4d810c01c847cba66f232db49
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-10 13:52:26 +0530

    Update dependencies and add Auth0 login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-2mgbgxm3b0scb3xq.us.auth0.com"
+    clientId="YwDGvHX9SFvedfN1ZnnlNmy0GQryyfdf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
