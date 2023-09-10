1fb3ffb17021e9b4e458c655a0f1c7658ae080ba
vaishali <vaishalirdj14@gmail.com>
2023-09-11 01:42:02 +0530

    child interface color palette changed

--- src/AutismAppComponents/ButtonPrimary.js
+++ src/AutismAppComponents/ButtonPrimary.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ButtonPrimary = (props) => {
     return (
         <div className="relative w-fit mt-[-1.00px] text-[20px] whitespace-nowrap">
-            <div className="relative bg-gradient-to-r from-oliveGreen to-green text-white border-2 border-[#606C38] hover:transform hover:scale-105 hover:rotate-1 hover:shadow-lg transition duration-300 ease-in-out px-[24px] py-[18px] rounded-md">
+            <div className="relative bg-gradient-to-r from-[#12e0b7ab] to-[#0d2cf48a] text-white border-2 border-[#12e0b77a] hover:transform hover:scale-105 hover:rotate-1 hover:shadow-lg transition duration-300 ease-in-out px-[24px] py-[18px] rounded-md">
                 <div className='flex'>
                     {props.text}
                 </div>
